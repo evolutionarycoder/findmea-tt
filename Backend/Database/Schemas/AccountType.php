8956595bb4ef38498b96ee90e25bf537b5b23b52
Daniel Prince <programmer96@hotmail.com>
2016-04-10 19:23:33 -0400

    Schemas for tables

---
+++ Backend/Database/Schemas/AccountType.php
@@ -0,0 +1,70 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Daniel M. Prince
+     * Date: 4/10/16
+     * Time: 7:11 PM
+     */
+
+    namespace Backend\Database\Schemas;
+
+
+    class AccountType {
+        public $id, $name, $slug;
+
+        /**
+         * AccountType constructor.
+         *
+         * @param int    $id   ID of the Account Type row
+         * @param string $name Name of the Account Type
+         * @param string $slug Shortened name for the Account Type
+         */
+        public function __construct($id, $name, $slug) {
+            $this->id   = $id;
+            $this->name = $name;
+            $this->slug = $slug;
+        }
+
+        /**
+         * @return int
+         */
+        public function getId() {
+            return $this->id;
+        }
+
+        /**
+         * @param int $id
+         */
+        public function setId($id) {
+            $this->id = $id;
+        }
+
+        /**
+         * @return string
+         */
+        public function getName() {
+            return $this->name;
+        }
+
+        /**
+         * @param string $name
+         */
+        public function setName($name) {
+            $this->name = $name;
+        }
+
+        /**
+         * @return string
+         */
+        public function getSlug() {
+            return $this->slug;
+        }
+
+        /**
+         * @param string $slug
+         */
+        public function setSlug($slug) {
+            $this->slug = $slug;
+        }
+
+    }
---
+++ Backend/Database/Schemas/Comment.php
@@ -0,0 +1,96 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Daniel M. Prince
+     * Date: 4/10/16
+     * Time: 7:15 PM
+     */
+
+    namespace Backend\Database\Schemas;
+
+
+    class Comment {
+        /**
+         * @var int
+         */
+        public $id, $locationId, $userId;
+
+        /**
+         * @var string
+         */
+        public $comment;
+
+        /**
+         * Comment constructor.
+         *
+         * @param int    $id         ID of the row in which the comment belongs to
+         * @param int    $locationId The location in which the user commented on
+         * @param int    $userId     The user who commented
+         * @param string $comment    The comment
+         */
+        public function __construct($id, $locationId, $userId, $comment) {
+            $this->id         = $id;
+            $this->locationId = $locationId;
+            $this->userId     = $userId;
+            $this->comment    = $comment;
+        }
+
+
+        /**
+         * @return int
+         */
+        public function getId() {
+            return $this->id;
+        }
+
+        /**
+         * @param int $id
+         */
+        public function setId($id) {
+            $this->id = $id;
+        }
+
+        /**
+         * @return int
+         */
+        public function getLocationId() {
+            return $this->locationId;
+        }
+
+        /**
+         * @param int $locationId
+         */
+        public function setLocationId($locationId) {
+            $this->locationId = $locationId;
+        }
+
+        /**
+         * @return int
+         */
+        public function getUserId() {
+            return $this->userId;
+        }
+
+        /**
+         * @param int $userId
+         */
+        public function setUserId($userId) {
+            $this->userId = $userId;
+        }
+
+        /**
+         * @return string
+         */
+        public function getComment() {
+            return $this->comment;
+        }
+
+        /**
+         * @param string $comment
+         */
+        public function setComment($comment) {
+            $this->comment = $comment;
+        }
+
+
+    }
