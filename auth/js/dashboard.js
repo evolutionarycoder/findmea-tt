fb34d6f9b6d0913fa8a902f6bc50448dfc008237
Daniel Prince <programmer96@hotmail.com>
2016-04-12 14:48:17 -0400

    front end files

--- auth/js/dashboard.js
+++ auth/js/dashboard.js
@@ -1,13 +1,16 @@
 /**
  * Created by Daniel M. Prince on 4/11/16.
  */
-var map;
-(function () {
-    map = new google.maps.Map(document.querySelector("#mapContainer"), {
-        center : {
-            lat: 10.536421,
-            lng: -61.311951
-        },
-        zoom   : 10
-    });
-})();
+var map             = GMaps("#mapContainer"),
+    locationSuccess = function (position) {
+        map.addMarker({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        }, "This is Me", {id : "user"});
+    };
+
+
+// Get User Location
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(locationSuccess);
+}
