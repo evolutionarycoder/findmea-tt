e7a58254af2ff36520a557554ca1a55a00d53c24
Daniel Prince <programmer96@hotmail.com>
2016-04-13 13:34:42 -0400

    modified it to use the Auth class and see if a users login credentials is good

--- api/users/index.php
+++ api/users/index.php
@@ -5,9 +5,10 @@
      * Date: 4/12/16
      * Time: 8:24 PM
      */
-
+    session_start();
     require '../../Backend/vendor/autoload.php';
 
+    use Backend\Authorization\Auth;
     use Backend\Database\Database;
     use Backend\Database\Schemas\User;
     use Backend\Database\Tables\Users;
@@ -16,24 +17,28 @@
     use Backend\ServerResponse\UserResponse;
 
     $global    = require '../global.php';
+    $auth      = new Auth();
     $directory = new FileSystem();
     $table     = new Users();
     $response  = new UserResponse(Response::STATUS_FAIL, Response::FAIL_TEXT);
 
     if (isset($_POST['type'])) {
         $type     = trim($_POST[Database::ACTION_TYPE]);
-        $fname    = trim($_POST[Users::FNAME]);
-        $lname    = trim($_POST[Users::LNAME]);
         $email    = trim($_POST[Users::EMAIL]);
         $password = trim($_POST[Users::PASSWORD]);
-        $account  = (int)trim($_POST[Users::ACCOUNT_TYPE]);
 
-        if ($table->emailExists($email)) {
-            $response->setText('Email Already Exists');
-            $type = '';
-        }
 
         if ($type === "create") {
+            // check email if it exists
+            if ($table->emailExists($email)) {
+                $response->setText('Email Already Exists');
+                $type = '';
+            }
+
+            $fname   = trim($_POST[Users::FNAME]);
+            $lname   = trim($_POST[Users::LNAME]);
+            $account = (int)trim($_POST[Users::ACCOUNT_TYPE]);
+
             $prefix = $fname . $lname . $email;
             $dir    = "system/users/{$prefix}";
             $user   = new User(null, $account, null, null, $fname, $lname, $email, $password, $prefix);
@@ -41,12 +46,20 @@
             if ($directory->mkdir($dir)) {
                 $id = $table->create($user);
                 if (is_numeric($id)) {
-                    $response->setStatus(Response::STATUS_OK);
-                    $response->setText(Response::OK_TEXT);
+                    $response->allGood();
                 } else {
                     $directory->rmdir($dir);
                 }
             }
         }
+
+
+        if ($type === 'login') {
+            $response->setText('Invalid Credentials');
+            if ($auth->logIn($email, $password, $table->getConnection())){
+                $response->allGood();
+            }
+        }
+
         echo $response->makeMeJson();
     }
