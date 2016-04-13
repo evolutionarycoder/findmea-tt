e292da416717f6ed5a6cbb571fe284f77ff187ce
Daniel Prince <programmer96@hotmail.com>
2016-04-13 14:20:05 -0400

    added prefix variable

--- auth/index.php
+++ auth/index.php
@@ -1169,6 +1169,9 @@
         <script type="text/javascript" src="assets/js/bootstrap.js"></script>
         <script type="text/javascript" src="assets/js/app.js"></script>
         <script type="text/javascript" src="assets/js/load.js"></script>
+        <script>
+            var prefix;
+        </script>
         <script type="text/javascript" src="assets/js/main.js"></script>
 
         <!-- Button Styles -->
