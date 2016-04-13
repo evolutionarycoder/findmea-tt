8bab7789ad996f72a5aa9f45ce55d24660d303b7
Daniel Prince <programmer96@hotmail.com>
2016-04-13 18:47:15 -0400

    added method template for clicking a filter

--- auth/js/dashboard.js
+++ auth/js/dashboard.js
@@ -1,10 +1,16 @@
 /**
  * Created by Daniel M. Prince on 4/11/16.
  */
-var map = GMaps("#mapContainer");
-
-
-// Get User Location
-if (navigator.geolocation) {
-    Loc(map).getAndAddLocationToMap();
+function initMap() {
+    var map = GMaps("#mapContainer");
+    // Get User Location
+    if (navigator.geolocation) {
+        Loc(map).getAndAddLocationToMap();
+    }
 }
+
+
+// when a filter is clicked
+$('#dropdown-tr li').click(function (e) {
+    e.preventDefault();
+});
