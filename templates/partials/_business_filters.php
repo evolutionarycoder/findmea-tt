cb72eb06823960b0f1c07cabee1e0751d29f2669
Daniel Prince <programmer96@hotmail.com>
2016-04-13 18:47:48 -0400

    file to load all business items

---
+++ templates/partials/_business_filters.php
@@ -0,0 +1,19 @@
+<?php
+    use Backend\Database\Tables\BusinessTypes;
+
+    function createBusinessTypeListItem($id, $name, $icon) {
+        echo "<li data-id='{$id}'>
+                <a href=\"#\">
+                    <span class=\"{$icon} margin-iconic\"></span>{$name}
+                </a>
+            </li>";
+    }
+
+    $table        = new BusinessTypes();
+    $businessData = $table->readAll();
+    createBusinessTypeListItem(0, 'All', 'entypo-plus-circled');
+    for ($i = 0; $i < count($businessData); $i++) {
+        $c = $businessData[$i];
+        createBusinessTypeListItem($c->getId(), $c->getName(), $c->getIcon());
+    }
+?>
