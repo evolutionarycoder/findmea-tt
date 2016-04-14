f8a0641b92144e88dcdf702d826db0619ffc3ce2
Daniel Prince <programmer96@hotmail.com>
2016-04-13 22:47:28 -0400

    wrap dashboard code in anonymous function

--- auth/js/pages/dashboard/dashboard.js
+++ auth/js/pages/dashboard/dashboard.js
@@ -1,16 +1,44 @@
 /**
  * Created by Daniel M. Prince on 4/11/16.
  */
-function initMap() {
-    var map = GMaps("#mapContainer");
-    // Get User Location
-    if (navigator.geolocation) {
-        Loc(map).getAndAddLocationToMap();
-    }
-}
+(function (global) {
+    var map,
+        manage = HttpManager('locations/index.php');
+
+    var initialize = function () {
+        map = GMaps("#mapContainer");
+        // Get User Location
+        if (navigator.geolocation) {
+            Loc(map).getAndAddLocationToMap();
+        }
+        manage.getJSON({
+            type: 'fetch'
+        }, function (locations) {
+            $.each(locations, function (inx, location) {
+                location['showing'] = true;
+                map.addMarker({
+                    lat: location['lat'],
+                    lng: location['lng']
+                }, location['name'], location, null, 'click', function (e) {
+                    var businessCardInfo = $('#business-card-info');
+                    businessCardInfo.css('display', 'none');
+
+                    Dom.delay(200, function () {
+                        // businessCardInfo.addClass('shake');
+                        businessCardInfo.css('display', 'block');
+                    });
+
+                    $('#business-name').text(this.name);
+                    $('#business-desc').text(this.desc);
+                    $('#business-area').text(this.area);
+                    $('#business-phone').text(this.phone);
+                    $('#business-site').text(this.website);
+                });
+            })
+        });
+        MapFilter(map);
+    };
 
 
-// when a filter is clicked
-$('#dropdown-tr li').click(function (e) {
-    e.preventDefault();
-});
+    global.initMap = initialize;
+})(window);
